d78c088d8d44978814d1d96991d006c9e1501387
Velin Poznakov <poznakovv@gmail.com>
2026-01-11 01:29:40 +0200

    one page setted

--- ToDoApp.Maui/MauiProgram.cs
+++ ToDoApp.Maui/MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using ToDoApp.Maui.Services;
+using ToDoApp.Maui.Services.Interfaces;
+using ToDoApp.Maui.ViewModels;
 
 namespace ToDoApp.Maui;
 
@@ -14,6 +17,10 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+		
+		builder.Services.AddSingleton<ITodoApiService, TodoApiService>();
+		builder.Services.AddTransient<TodosViewModel>();
+
 
 #if DEBUG
 		builder.Logging.AddDebug();
