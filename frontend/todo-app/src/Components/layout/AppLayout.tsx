639e5b372cf9fca6c72c114b64b43910d3491626
Velin Poznakov <vpoznakov@tu-sofia.bg>
2026-04-22 00:53:30 +0300

    Initial project architecture

--- frontend/todo-app/src/Components/layout/AppLayout.tsx
+++
@@ -1,5 +0,0 @@
-function AppLayout(){
-    return <div>App Layout</div>
-}
-
-export default AppLayout;
--- frontend/todo-app/src/swagger.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  swaggerUrl: 'http://localhost:5166/swagger/v1/swagger.json',
-  output: './src/api',
-  fileName: 'todo-api.ts',
-  httpClient: 'fetch', // or 'axios'
-};
