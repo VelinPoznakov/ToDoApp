548ff589309269de396c313aeade2a78cdd113f6
Velin Poznakov <PoznakovV@gmail.com>
2026-07-17 01:19:39 +0300

    SupportRepository.cs with get all method

---
+++ backend/TodoApp.Data/Repositories/Contracts/ISupportRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using TodoApp.Models.Data;
+
+namespace TodoApp.Data.Repositories.Contracts;
+
+public interface ISupportRepository
+{
+    Task<IEnumerable<SupportMessage>> GetAllSupportMessagesNoTracking(
+            Expression<Func<SupportMessage, bool>>? filter,
+            Expression<Func<SupportMessage, SupportMessage>> projection,
+            bool ignoreQueryFilter = false,
+            bool includeUser = false);
+}
---
+++ backend/TodoApp.Data/Repositories/SupportRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Data.Repositories.Contracts;
+using TodoApp.Models.Data;
+
+namespace TodoApp.Data.Repositories;
+
+public class SupportRepository: BaseRepository, ISupportRepository
+{
+    protected SupportRepository(TodoDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<IEnumerable<SupportMessage>> GetAllSupportMessagesNoTracking(
+        Expression<Func<SupportMessage, bool>>? filter,
+        Expression<Func<SupportMessage, SupportMessage>>? projection,
+        bool ignoreQueryFilter = false,
+        bool includeUser = false)
+    {
+        IQueryable<SupportMessage> messages = DbContext
+            .SupportMessages
+            .AsNoTracking();
+
+        if (ignoreQueryFilter)
+        {
+            messages = messages.IgnoreQueryFilters();
+        }
+
+        if (includeUser)
+        {
+            messages = messages.Include(u => u.ApplicationUser);
+        }
+
+        if (filter != null)
+        {
+            messages = messages.Where(filter);
+        }
+
+        messages = messages
+            .OrderBy(s => s.CreatedOn)
+            .ThenBy(s => s.Title);
+
+        if (projection != null)
+        {
+            messages = messages.Select(projection);
+        }
+
+        return await messages.ToArrayAsync();
+    }
+}
